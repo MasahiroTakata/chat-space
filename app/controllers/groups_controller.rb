9686ff01ec6188035fc4a0577034a304c2160a01
Masahiro Takata <takata005581016@gmail.com>
2020-02-26 12:50:13 +0900

    Update groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,3 +1,35 @@
 class GroupsController < ApplicationController
+  def create
+    # フォームから送信されるカラムを確認する
+    # binding.pry
+    # 登録は、newでもcreateでも可能
+    @group = Group.new(group_params)
 
+    # 保存が出来たかどうかの分岐
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    # 編集したグループの情報を取得
+    @group = Group.find(params[:id])
+
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :edit
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
+  end
 end
