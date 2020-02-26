c833a6fde61eb6b7b30e629a6b3c2cc6789c5863
Masahiro Takata <takata005581016@gmail.com>
2020-02-26 12:47:57 +0900

    Update groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,3 +1,10 @@
 class GroupsController < ApplicationController
+  def index
+  end
 
+  def new
+    @group = Group.new
+    # ログインユーザを、グループに登録する（配列に要素を追加する）
+    @group.users << current_user
+  end
 end
