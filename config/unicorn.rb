7cde18ae4bec735d92317d45b9377427458d69a6
Masahiro Takata <takata005581016@gmail.com>
2020-03-04 17:41:35 +0900

    configのunicornの設定変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,7 +11,8 @@ working_directory app_path
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
-listen 3000
+# listen 3000 2020/3/4 を以下に変更
+listen "#{app_path}/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
