5202729da6cf391702dd65336331038bf3bb9147
Masahiro Takata <takata005581016@gmail.com>
2020-03-04 14:52:41 +0900

    AWSで世界配信

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,9 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # 2020/3/4 コメントアウト↓↓
+  # config.assets.js_compressor = :uglifier
+
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
